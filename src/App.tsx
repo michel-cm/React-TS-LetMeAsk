803c8ee4f3ffcb36f4c6c19b11fabaed66e80ef1
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-07-31 16:50:02 -0300

    navegação Home para New Home e vice versa feita.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+type Exact = {
+  exact: boolean;
+}
 
 function App() {
   return (    
-      <Home />   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/rooms/new" element={<NewRoom />}></Route>
+      </Routes> 
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,22 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import  illustration  from '../assets/images/illustration.svg';
 import  logoImg  from '../assets/images/logo.svg';
 import  googleIconImg  from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-import '../styles/auth.scss';
-
-export function Home(){
-
-    const navigate = useNavigate();
-
-    function navigateToNewRoom() {
-        navigate('/rooms/new');
-    }
-
-    return (
+export function NewRoom() {
+    return ( 
         <div id='page-auth'>
             <aside>
                 <img src={illustration} alt="Ilustração simbolizando a aplicação" />
@@ -26,20 +18,18 @@ export function Home(){
             <main>
                 <div className='main-content'>
                     <img src={logoImg} alt="letmeask" />
-                    <button onClick={navigateToNewRoom} className='create-room'>
-                        <img src={googleIconImg} alt="logo do Google" />
-                        crie sua sala com o Google
-                    </button>
-                    <div className='separator'>ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>                  
                     <form >
                         <input 
                             type="text" 
-                            placeholder='Digite o código da sala'
+                            placeholder='Nome da sala'
                         />
-                        <Button type='submit'>Entrar na sala</Button>
+                        <Button type='submit'>Criar sala</Button>
                     </form>
+                    <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link> </p>
                 </div>
             </main>
         </div>
     );
 }
+  
